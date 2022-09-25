01b69f0c4bdcec80ea87b2a94e888c693c14799b
KozlovaVP <kozlovavp95@gmail.com>
2022-09-25 11:07:23 +0300

    The fourth solution

--- 3_3.py
+++ 3_3.py
@@ -7,5 +7,5 @@ print('Hello ' + name + ' (' + age + ' years old, from ' + city + ')!')
 print('Hello %s (%s years old, from %s)!' % (name, age, city))
 # the third solution:
 print('Hello {} ({} years old, from {})!'.format(name, age, city))
-# the forth solution:
+# the fourth solution:
 print(f'Hello {name} ({age} years old, from {city})!')
