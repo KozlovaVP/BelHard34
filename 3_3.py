a72fd53f8bb968facfef472167721f1f0b0276e2
KozlovaVP <kozlovavp95@gmail.com>
2022-09-25 11:04:33 +0300

    The third solution

--- 3_3.py
+++ 3_3.py
@@ -5,3 +5,5 @@ city = input('Input a city: ')
 print('Hello ' + name + ' (' + age + ' years old, from ' + city + ')!')
 # the second solution:
 print('Hello %s (%s years old, from %s)!' % (name, age, city))
+# the third solution:
+print('Hello {} ({} years old, from {})!'.format(name, age, city))
