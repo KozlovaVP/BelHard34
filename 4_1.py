b4f852c2e669700e911da025e3a194a3558d5a67
KozlovaVP <kozlovavp95@gmail.com>
2022-09-28 14:12:45 +0300

    Another solution

--- 4_1.py
+++ 4_1.py
@@ -1,7 +1,3 @@
 number = int(input('Input a number: '))
-i = 1
-list_of_2 = []
-while i <= number:
-    list_of_2.append(pow(2, i))
-    i +=1
+list_of_2 = [pow(2, i) for i in range(number + 1)]
 print(list_of_2)
