d80c3f48e7ea69493d5b780627be4aa8aa340dfc
KozlovaVP <kozlovavp95@gmail.com>
2022-09-28 17:13:59 +0300

    The solution

--- 5_4.py
+++ 5_4.py
@@ -15,3 +15,21 @@ for i in range(len(text2)):
     char = bin(ord(text2[i]))[2:]
     char = "0" * (8 - len(char)) + char
     print(char, end='\t')
+print(end='\n')
+
+result = ''
+for i in range(len(text2)):
+    char1 = bin(ord(text1[i]))[2:]
+    char1 = "0" * (8 - len(char1)) + char1
+    char2 = bin(ord(text2[i]))[2:]
+    char2 = "0" * (8 - len(char2)) + char2
+    for k in range(len(char1) + 1):
+        if not k % 8 and k != 0:
+            result += '\t'
+        elif char1[k] == char2[k]:
+            result += '0'
+        else:
+            result += '1'
+print('Result:')
+print(result)
+
