eefa979986a8a4fe7cff6a7cadc77f42583fdbd6
KozlovaVP <kozlovavp95@gmail.com>
2022-09-28 15:14:01 +0300

    The solution

---
+++ 5_2.py
@@ -0,0 +1,4 @@
+# Сделать калькулятор: у пользователя спрашивается число, потом действие и второе число
+number1 = int(input('Insert the first number: '))
+operation1 = int(input('Insert m: '))
+k = int(input('Insert k: '))
