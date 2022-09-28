dbcc779297f7536d24fc6c6023b592c479402b15
KozlovaVP <kozlovavp95@gmail.com>
2022-09-28 15:15:43 +0300

    The count was removed

--- 5_3.py
+++ 5_3.py
@@ -1,9 +1,7 @@
 # **Вывести четные числа от 2 до N по 5 в строку
 n = int(input('Insert n: '))
-count = 0
 for i in range(2, n + 1, 2):
     print(i, end = '\t')
-    count += 1
     if not i % 5:
         print(end = '\n')
 
