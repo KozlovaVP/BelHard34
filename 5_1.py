a83a2c951da1623f27f87b8b2d4e8b0bd78f3030
KozlovaVP <kozlovavp95@gmail.com>
2022-09-28 15:01:02 +0300

    The solution

---
+++ 5_1.py
@@ -0,0 +1,12 @@
+# Вывести первые N чисел кратные M и больше K
+n = int(input('Insert n: '))
+m = int(input('Insert m: '))
+k = int(input('Insert k: '))
+count = 0
+result = []
+i = k
+while count < n:
+    i += 1
+    if not i % m:
+        print(i, end = '\t')
+        count += 1
